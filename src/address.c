1d981306d10a1f75f6629a9c25d2ee1c84f428c4
hakolao <okkohakola@gmail.com>
2020-02-27 16:22:29 +0200

    Clean address parsing

--- src/address.c
+++ src/address.c
@@ -6,22 +6,17 @@
 /*   By: ohakola <ohakola@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/27 13:07:57 by ohakola           #+#    #+#             */
-/*   Updated: 2020/02/27 15:49:56 by ohakola          ###   ########.fr       */
+/*   Updated: 2020/02/27 16:22:12 by ohakola          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-#include <stdio.h>
 
 char					*parse_address(t_printf *data)
 {
 	char					*hex;
-	char					*beg;
-	char					*res;
 
 	hex = ft_itoa_uintmax_base(va_arg(data->variables, long long int), 16);
-	beg = "0x";
-	res = ft_strjoin(beg, hex);
 	ft_strdel(&hex);
-	return (res);
+	return (ft_strjoin("0x", hex));
 }
