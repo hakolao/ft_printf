818e6535cb10c60f199c9dc00941fa251bf1a721
hakolao <okkohakola@gmail.com>
2020-09-01 00:14:07 +0300

    Add dragon4 comments

--- libdtoa/dragon4.c
+++ libdtoa/dragon4.c
@@ -6,7 +6,7 @@
 /*   By: ohakola <ohakola@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/17 19:27:50 by ohakola           #+#    #+#             */
-/*   Updated: 2020/08/31 20:56:24 by ohakola          ###   ########.fr       */
+/*   Updated: 2020/09/01 00:13:25 by ohakola          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,6 +49,37 @@ static int32_t		get_digit_exponent(t_dragon4_params params)
 ** Dragon4 implementation using the algorithm laid out by ryanjuckett in
 ** http://www.ryanjuckett.com/programming/printing-floating-point-numbers/
 ** Here implemented in C and split into small logical functions.
+** 1. Compute initial values for
+**  value     = scaled_value / scale
+**  margin_low = scaled_margins[0] (low) / scale
+** 2. Compute the digit exponent (base 10 exponent: e.g. 1.34*10^digit_exponent)
+** 3. Based on digit exponent estimation, adjust scale, scaled_value & margins
+** 4. Perform big integer divisions on outputdigit = scaled_value / scale
+**    until we hit the cutoff or are far enough away from our margins.
+** Basically this is an optimized version of following algorthim:
+** ================================
+** 1. Compute digit exponent
+** digit_exponent = (int)floor( log10(value) ); e.g. 123.45 would compute 2
+** 2. Compute initial values for numerator and denominator where
+** value = numerator / denominator
+** if (exponent > 0)
+**  value = (value_numerator = mantissa * 2^exponent) / (value_denominator = 1)
+** else
+**  value = (value_numerator = mantissa) / (value_denominator = 2^(-exponent))
+** 3. Scale numerator or denominator so that first digit is in one's place
+** e.g. 123.123 => 1.23123
+** if (digit_exponent > 0)
+**  value_denominator = value_denominator * 10^digit_exponent
+** else
+**  value_numerator = value_numerator * 10^digit_exponent
+** 4. Compute rest of the digits:
+** while (value_numerator > 0.0) // Or cutoff if hit's cutoff  limit
+**  digit = value_numerator / value_denominator
+**  buffer[num_digits++] = (char)digit + '0'
+**  // remainder:
+**  value_numerator = value_numerator - digit * value_denominator
+**  // scale up so that first digit is in one's place
+**  value_numerator = value_numerator * 10
 */
 
 uint32_t			dragon4(t_dragon4_params params)
